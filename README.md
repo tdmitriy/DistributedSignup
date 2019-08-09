4dfbbccaf24751d85846d281baa7c01cabfbd831
hasw3ll <haswelldt@gmail.com>
2019-08-09 14:09:26 +0300

    add gradle dependencies, docker plugin

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# DistributedSignup
+# Distributed Signup
 Test task - system allows to signup a player and store his data in persistence storage (REST + Kafka)
---
+++ signup-service/src/test/java/com/newage/signupservice/SignupServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.newage.signupservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SignupServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
