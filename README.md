b0fb582d88a4e0627d319c5a0dc56d53fc219558
hasw3ll <haswelldt@gmail.com>
2019-08-11 23:48:34 +0300

    update README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Distributed Signup
 Test task - system allows to signup a player and store his data in persistence storage (REST + Kafka)
+
+# How to run
+1. Clone the project
+2. Install docker-compose
+3. Run `sh start_app.sh` to build all microservices, docker images and also it runs docker compose command
+4. Wait for `docker-compose` build
+5. Run [http://localhost:8081/swagger-ui.html](http://localhost:8081/swagger-ui.html) and test functionality
+6. Run `sh stop_app.sh` to stop compose
--- persistence-service/src/test/java/com/newage/persistenceservice/PersistenceServiceApplicationTests.java
+++ persistence-service/src/test/java/com/newage/persistenceservice/PersistenceServiceApplicationTests.java
@@ -1,13 +1,9 @@
 package com.newage.persistenceservice;
 
 import com.newage.persistenceservice.model.entity.Player;
-import com.newage.persistenceservice.repository.PlayerRepository;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.Instant;
-import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.assertNotNull;
@@ -17,38 +13,18 @@ import static org.junit.Assert.assertTrue;
 public class PersistenceServiceApplicationTests extends AbstractApplicationTest {
 
     @Test
-    public void tt() {
+    public void playerRepositoryTest() {
+        Player player = new Player();
+        player.setEmail("xyi");
+        player.setPassword("xyi");
 
+        Player saved = playerRepository.save(player);
+        assertNotNull(saved);
+
+        Optional<Player> foundById = playerRepository.findById(saved.getId());
+        assertTrue(foundById.isPresent());
+
+        Optional<Player> foundByEmail = playerRepository.findById(saved.getId());
+        assertTrue(foundByEmail.isPresent());
     }
-
-//    @Test
-//    public void playerRepositoryTest() {
-//        Player player = new Player();
-//        player.setEmail("xyi");
-//        player.setPassword("xyi");
-//
-//        Player saved = playerRepository.save(player);
-//        assertNotNull(saved);
-//
-//        Optional<Player> found = playerRepository.findById(saved.getId());
-//        assertTrue(found.isPresent());
-//
-//        List<Player> all = playerRepository.findAll();
-//    }
-//
-//    @Test
-//    public void xx() {
-//        Player player = new Player();
-//        player.setEmail("sss");
-//        player.setPassword("sss");
-//
-//        Player saved = playerRepository.save(player);
-//        assertNotNull(saved);
-//
-//        Optional<Player> found = playerRepository.findById(saved.getId());
-//        assertTrue(found.isPresent());
-//
-//        List<Player> all = playerRepository.findAll();
-//    }
-
 }
